a81f021739abb6106df7e14b241d708a1c50e570
Kevin Ngo <66854579+KennyKnows@users.noreply.github.com>
2024-04-29 21:37:58 -0400

    Add files via upload

---
+++ Delete Temp Folder NOW.ps1
@@ -0,0 +1,11 @@
+﻿#Define the path to the temp directory that will be cleared.
+$tempDir = "C:\Users\Kkool\AppData\Local\Temp"
+
+#Check if it exists.
+if (Test-Path $tempDir) {
+Remove-Item -Path $tempDir\* -Force -Recurse
+Write-Host "All files and folders in $tempDir have been deleted."
+}
+else {
+Write-Host "The directory $tempDir does not exist."
+}
